f76795092f26b7698721e9d5b8ca25f07bb68d07
ciscorucinski <ciscorucinski@gmail.com>
2019-10-12 22:27:30 +0900

    added method entry point for cloud function

--- url_retriever/archieve_url_retriever.py
+++ url_retriever/archieve_url_retriever.py
@@ -1,15 +1,19 @@
 from Webpage import AndroidStudioArchive
 from file import UrlStorage
 
-archive_url = AndroidStudioArchive().request_page().download_page_url
-file_storage = UrlStorage("archieve_urls.txt").deserialize()
 
-if archive_url in file_storage.urls:
-    print("URL already in file")
-else:
-    file_storage.serialize(archive_url)
-    print("URL appended to file")
+def entry():
+    archive_url = AndroidStudioArchive().request_page().download_page_url
+    file_storage = UrlStorage("archive_urls.txt").deserialize()
 
-print(file_storage.last_updated, set(zip(file_storage.dates, file_storage.urls)))
+    if archive_url in file_storage.urls:
+        print("URL already in file")
+    else:
+        file_storage.serialize(archive_url)
+        print("URL appended to file")
 
-# TODO parse new file
+    print(file_storage.last_updated, set(zip(file_storage.dates, file_storage.urls)))
+
+
+if __name__ == '__main__':
+    entry()
