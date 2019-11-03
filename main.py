0a7dd235c3363c0fb4b5e7b8fd7a209ac49722b2
ciscorucinski <ciscorucinski@gmail.com>
2019-11-03 21:26:35 +0900

    updated sample usage

--- main.py
+++ main.py
@@ -31,15 +31,16 @@ def main():
 if __name__ == '__main__':
     main()
 
-    print("latest", Release.latest_releases())
-    print("latest (Stable)", Release.latest_releases(stable_only=True))
-    print("4.0", Release.releases("4.0"))
-    print("3.6", Release.releases("3.6"))
-    print("3.5", Release.releases("3.5"))
-    print("3.4", Release.releases("3.4"))
-    print("3.3", Release.releases("3.3"))
-    print("3.2", Release.releases("3.2"))
-    print("3.1", Release.releases("3.1"))
-    print("3.0", Release.releases("3.0"))
-    print("2.4", Release.releases("2.4"))   # Note: Has "preview" key
-    print("2.3", Release.releases("2.3"))
+    print()
+    print("All Releases by Version")
+    for version in Release.all_versions(Release.Canary):
+        print(version, Release.releases(version))
+
+    print()
+    print("Data by version then release")
+    for version in Release.all_versions(Release.Canary):
+        print("-" * 120)
+        for release in Release:
+            print(version, "|", release, Release.data(version, release))
+
+
