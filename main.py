2ad0957b43a8d6ba28b0693586f84f2739ba321f
ciscorucinski <ciscorucinski@gmail.com>
2019-11-09 00:53:01 +0900

    save and retrieve data locally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from AndroidStudio import AndroidStudioPage
 from Release import Release
-from util.file import UrlStorage
+from util.file import LastUpdatedStorage, ReleaseInfoStorage
 
 
 def organize_releases(studio_releases):
@@ -12,20 +12,45 @@ def organize_releases(studio_releases):
         Release.add(release_type, version, download_link)
 
 
+def release_info():
+    releases = []
+    latest = []
+    for version in Release.all_versions(include_latest=True):
+        for release in reversed(Release):
+            data = Release.data(version, release)
+            if data is not None:
+                if version == "latest":
+                    latest.append(tuple([data[0], release.name, data[1]]))
+                else:
+                    releases.append(tuple([version, release.name, data]))
+                    break
+            else:
+                continue
+
+    return releases, latest
+
+
 def main():
     webpage = AndroidStudioPage().request_page()
-    file_storage = UrlStorage("archive_urls.txt").read()
+    last_updated_file = LastUpdatedStorage("archives.txt")
+    release_info_file = ReleaseInfoStorage("releases.txt")
 
     download_url = webpage.get_download_info_url()
 
-    if download_url in file_storage.urls:
+    if download_url in last_updated_file.read().urls:
         print("URL already exists:", download_url)
+        releases = release_info_file.read()
+
+        for version, release, download_url in releases:
+            Release.add(release, version, download_url)
+
     else:
-        file_storage.append(download_url)
+        last_updated_file.append(download_url)
         print("URL appended:", download_url)
 
         android_studio_download_info = webpage.parse_downloads()
         organize_releases(android_studio_download_info)
+        release_info_file.write(release_info())
 
 
 if __name__ == '__main__':
@@ -33,14 +58,14 @@ if __name__ == '__main__':
 
     print()
     print("All Releases by Version")
-    for version in Release.all_versions(Release.Canary):
+    for version in Release.all_versions():
         print(version, Release.releases(version))
 
     print()
     print("Data by version then release")
-    for version in Release.all_versions(Release.Canary):
+    for version in Release.all_versions():
         print("-" * 120)
         for release in Release:
-            print(version, "|", release, Release.data(version, release))
-
-
+            url = Release.data(version, release)
+            if url is not None:
+                print(version, "|", release, Release.data(version, release))
--- util/file.py
+++ util/file.py
@@ -1,18 +1,46 @@
+from collections import OrderedDict
 from datetime import datetime
 
 
-class UrlStorage:
+class ReleaseInfoStorage:
+    versions = []
+    releases = []
+    download_links = []
+
+    def __init__(self, file):
+        self.filename = file
+
+    def write(self, releases, latest):
+        lines = OrderedDict()
+        with open(self.filename, "w") as file:
+            for release in releases:
+                lines.update({f"{release[0]} {release[1]} {release[2]}\n": None})
+            for release in latest:
+                lines.update({f"{release[0]} {release[1]} {release[2]}\n": None})
+            for line in lines:
+                file.write(line)
+        return self
+
+    def read(self):
+        releases = []
+        with open(self.filename, "r") as file:
+            for line in file:
+                releases.append(tuple(line[:-1].split(" ")))
+        return releases
+
+
+class LastUpdatedStorage:
     dates = []
     urls = []
     last_updated = None
 
     date_format = "%Y-%m-%d %H:%M:%S"
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, file):
+        self.filename = file
 
     def read(self):
-        with open(self.name, "r") as file:
+        with open(self.filename, "r") as file:
             for line in file:
                 date, time, url = line.split(" ")
                 self.last_updated = datetime.strptime(f"{date} {time}", self.date_format)
@@ -21,7 +49,7 @@ class UrlStorage:
         return self
 
     def append(self, url):
-        with open(self.name, "a") as file:
+        with open(self.filename, "a") as file:
             time = datetime.now()
             file.writelines([
                 time.strftime(self.date_format),
